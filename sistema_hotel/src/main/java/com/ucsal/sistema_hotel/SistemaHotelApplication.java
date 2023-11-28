dd91a3d575c30c6b6cab553ed7e4d08bbc327ba1
joaodaniel1604 <143419902+joaodaniel1604@users.noreply.github.com>
2023-11-28 08:46:46 -0300

    Add files via upload

---
+++ sistema_hotel/src/main/java/com/ucsal/sistema_hotel/SistemaHotelApplication.java
@@ -0,0 +1,13 @@
+package com.ucsal.sistema_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHotelApplication.class, args);
+	}
+
+}
